7b7b76ec6f2f2b0d4aa2da114b3789e18468f99b
Emiliano Grilli <emiliano.grilli@gmail.com>
2025-08-20 12:03:11 +0200

    removed custom form init.el

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; Minimal config
+; Minimal config
 (use-package emacs
   :init
   (column-number-mode 1)
@@ -146,17 +146,4 @@
      (t . (1.3))))
 (setq modus-themes-to-toggle '(modus-operandi-tinted modus-vivendi-tinted))
 (load-theme 'modus-vivendi-tinted)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/.emacsMinimal/README.org")))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 
